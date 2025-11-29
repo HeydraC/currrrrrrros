23e75bd22081138ffd13443ef2aa545be37f1f71
Carlos Heydra <heydracarlos7@gmail.com>
2025-11-28 22:42:55 -0400

    Implement initialBoard function in Proyecto1.hs
    Add initialBoard function to create game board.

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -14,3 +14,13 @@ verify _ [] = True
 verify (o1,(i1,j1),l1) ((o2,(i2,j2),l2) : xs)
   | null [x | x<-positions (o1,(i1,j1),l1), elem x (positions (o2,(i2,j2),l2))] = verify (o1,(i1,j1),l1) xs
   | otherwise = False
+
+-- Pregunta 1
+initialBoard :: [Vehicle] -> Board
+initialBoard [] = []
+initialBoard [x] = [x]
+initialBoard (x:xs)
+  | verify x xs = if null (initialBoard xs) then [] else x:xs
+  | otherwise = []
+
+main = putStrLn $ show $ initialBoard [(H, (2,0), 2), (V, (0,2), 3)]
